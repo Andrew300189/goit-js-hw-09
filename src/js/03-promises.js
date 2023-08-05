83429cbce22c6f4ba02447ec3701559fa48cc28f
Andrey <94688521+Andrew300189@users.noreply.github.com>
2023-08-05 14:43:24 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,6 +1,5 @@
 import Notiflix from 'notiflix';
 
-// Функция, которая создает и возвращает промисы с задержкой
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
@@ -17,16 +16,13 @@ function createPromise(position, delay) {
 function handleSubmit(event) {
   event.preventDefault();
 
-  // Получаем значения из полей формы
   const form = event.target;
   const initialDelay = parseInt(form.elements.delay.value);
   const step = parseInt(form.elements.step.value);
   const amount = parseInt(form.elements.amount.value);
 
-  // Очищаем консоль перед новыми сообщениями
   console.clear();
 
-  // Создаем массив промисов с заданными значениями и уникальными задержками
   const promises = Array.from({ length: amount }, (_, i) => {
     const position = i + 1;
     const delay = initialDelay + i * step;
@@ -40,12 +36,10 @@ function handleSubmit(event) {
     );
   });
 
-  // Ожидаем выполнения всех промисов и отображаем уведомления
   Promise.all(promises).then(() => {
     Notiflix.Notify.Info(`All promises resolved successfully.`);
   });
 }
 
-// Получаем форму и назначаем обработчик события сабмита
 const form = document.querySelector('.form');
 form.addEventListener('submit', handleSubmit);
